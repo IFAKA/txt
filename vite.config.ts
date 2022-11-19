2b0262804b03bdecf0d3a9f2d72e10b905c12ea6
IFAKA <me@faka.dev>
2022-11-19 13:27:28 -0300

    PWA manifest fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@
 
 import react from "@vitejs/plugin-react"
 import path from "path"
-import * as manifest from "./manifest.json"
 
 import { defineConfig } from "vite"
 import { VitePWA } from "vite-plugin-pwa"
@@ -13,7 +12,30 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      manifest,
+      manifest: {
+        name: "TXT",
+        short_name: "txt",
+        description: "Note application",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
       includeAssets: [
         "favicon.svg",
         "favicon.ico",
