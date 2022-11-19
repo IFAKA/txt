6c63263b6e630d1f5a6937c86a3a637a02de7d3c
IFAKA <me@faka.dev>
2022-11-19 13:23:07 -0300

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,34 @@
 /// <reference types='vitest'/>
 /// <reference types='vite/client'/>
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import react from "@vitejs/plugin-react"
+import path from "path"
+import * as manifest from "./manifest.json"
+
+import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest,
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      // switch to "true" to enable sw on development
+      devOptions: {
+        enabled: false,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html}", "**/*.{svg,png,jpg,gif}"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -15,7 +36,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts']
-  }
-});
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
+  },
+})
