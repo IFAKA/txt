f54d4bedcc86a137853956e16bac67862ccd7933
IFAKA <me@faka.dev>
2022-11-19 13:38:25 -0300

    PWA manifest fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,52 +5,10 @@ import react from "@vitejs/plugin-react"
 import path from "path"
 
 import { defineConfig } from "vite"
-import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      manifest: {
-        name: "TXT",
-        short_name: "txt",
-        description: "Note application",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-      includeAssets: [
-        "favicon.svg",
-        "favicon.ico",
-        "robots.txt",
-        "apple-touch-icon.png",
-      ],
-      // switch to "true" to enable sw on development
-      devOptions: {
-        enabled: false,
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html}", "**/*.{svg,png,jpg,gif}"],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
